4b1ebbef14d65bca996ca4dd408a50445b62dac2
DanielSandor84 <sandordaniel19840727@gmail.com>
2018-12-09 21:08:14 +0100

    VBO

--- App.cpp
+++ App.cpp
@@ -11,4 +11,18 @@ void App::Init()
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
+
+	// An array of 3 vectors which represents 3 vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+	   -1.0f, -1.0f, 0.0f,
+	   1.0f, -1.0f, 0.0f,
+	   0.0f,  1.0f, 0.0f,
+	};
+
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 }
--- App.h
+++ App.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <GL/glew.h>
+
 class App
 {
+	GLuint vertexbuffer; // This will identify our vertex buffer
 
 public:
 
