0b2c974f6e6112c3f096318f5823541995d8c56e
DanielSandor84 <sandordaniel19840727@gmail.com>
2018-12-09 06:24:15 +0100

    Class App added to the project

---
+++ App.cpp
@@ -0,0 +1,9 @@
+#include "App.h"
+
+#include <GL/glew.h>
+
+void App::Init()
+{
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+}
--- playground.cpp
+++ playground.cpp
@@ -8,6 +8,7 @@ GLFWwindow* window;
 
 #include <glm/glm.hpp>
 using namespace glm;
+#include "App.h"
 
 int main( void )
 {
@@ -47,8 +48,9 @@ int main( void )
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	App app;
+
+	app.Init();
 
 	do{
 		glClear(GL_COLOR_BUFFER_BIT);
