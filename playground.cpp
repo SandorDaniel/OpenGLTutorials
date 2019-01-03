08000ed8c0de1e382e9446001c0d5ce14978d7a0
DanielSandor84 <sandordaniel19840727@gmail.com>
2019-01-03 20:04:25 +0100

    Backface Culling

--- playground.cpp
+++ playground.cpp
@@ -80,7 +80,8 @@ int main( void )
 	 
 	glEnable(GL_DEPTH_TEST); // Enable depth test
 	glDepthFunc(GL_LESS); // Accept fragment if it closer to the camera than the former one
-	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE); // Cull triangles which normal is not towards the camera ...
+	glCullFace(GL_BACK); // ... ie let the GPU check if the camera is behind
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f); // Dark blue background
 
 	do{
