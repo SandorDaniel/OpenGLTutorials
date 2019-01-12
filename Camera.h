aa2771d9e2e2206dcbc9fd8403e55f406c57c3b9
DanielSandor84 <sandordaniel19840727@gmail.com>
2019-01-12 02:10:28 +0100

    Otcommenting Camera::upDate()

--- Camera.h
+++ Camera.h
@@ -226,118 +226,118 @@ public:
 
 	void upDate(GLFWwindow* window) {
 
-		//#pragma region Time
+		////#pragma region Time
 
-		// glfwGetTime is called only once, the first time this function is called
-		static double lastTime = glfwGetTime();
+		//// glfwGetTime is called only once, the first time this function is called
+		//static double lastTime = glfwGetTime();
 
-		// Compute time difference between current and last frame
-		double currentTime = glfwGetTime();
-		float deltaTime = float(currentTime - lastTime);
+		//// Compute time difference between current and last frame
+		//double currentTime = glfwGetTime();
+		//float deltaTime = float(currentTime - lastTime);
+
+		////#pragma endregion
+
+		//#pragma region Window
+
+		//int width, height;
+		//glfwGetWindowSize(window, &width, &height);
 
 		//#pragma endregion
 
-		#pragma region Window
+		//#pragma region Cursor - Orientation
 
-		int width, height;
-		glfwGetWindowSize(window, &width, &height);
+		//// Get mouse position
+		//static bool is_call_back_method_set_up_for_mouse_motion = false;
+		//if (!is_call_back_method_set_up_for_mouse_motion)
+		//{
+		//	glfwSetCursorPosCallback(window, InPut::Cursor::motionCallback);
+		//	is_call_back_method_set_up_for_mouse_motion = true;
+		//}
 
-		#pragma endregion
+		//// TODO: it may be skipped later 
+		//static bool is_mouse_inited = false;
+		//if (!is_mouse_inited)
+		//{
+		//	InPut::Cursor::motionCallback(window, width / 2, height / 2);
+		//	InPut::Cursor::regist(*this);
+		//	is_mouse_inited = true;
+		//}
 
-		#pragma region Cursor - Orientation
+		////// Compute new orientation
+		////horizontalAngle += mouseSpeed * static_cast<float>(width / 2 - InPut::Cursor::getXPos());
+		////verticalAngle += mouseSpeed * static_cast<float>(height / 2 - InPut::Cursor::getYPos());
 
-		// Get mouse position
-		static bool is_call_back_method_set_up_for_mouse_motion = false;
-		if (!is_call_back_method_set_up_for_mouse_motion)
-		{
-			glfwSetCursorPosCallback(window, InPut::Cursor::motionCallback);
-			is_call_back_method_set_up_for_mouse_motion = true;
-		}
+		//// Direction : Spherical coordinates to Cartesian coordinates conversion
+		//glm::vec3 direction(
+		//	cos(verticalAngle) * sin(horizontalAngle),
+		//	sin(verticalAngle),
+		//	cos(verticalAngle) * cos(horizontalAngle)
+		//);
 
-		// TODO: it may be skipped later 
-		static bool is_mouse_inited = false;
-		if (!is_mouse_inited)
-		{
-			InPut::Cursor::motionCallback(window, width / 2, height / 2);
-			InPut::Cursor::regist(*this);
-			is_mouse_inited = true;
-		}
+		//// Right vector
+		//glm::vec3 right = glm::vec3(
+		//	sin(horizontalAngle - 3.14f / 2.0f),
+		//	0,
+		//	cos(horizontalAngle - 3.14f / 2.0f)
+		//);
 
-		//// Compute new orientation
-		//horizontalAngle += mouseSpeed * static_cast<float>(width / 2 - InPut::Cursor::getXPos());
-		//verticalAngle += mouseSpeed * static_cast<float>(height / 2 - InPut::Cursor::getYPos());
+		//// Up vector
+		//glm::vec3 up = glm::cross(right, direction);
 
-		// Direction : Spherical coordinates to Cartesian coordinates conversion
-		glm::vec3 direction(
-			cos(verticalAngle) * sin(horizontalAngle),
-			sin(verticalAngle),
-			cos(verticalAngle) * cos(horizontalAngle)
-		);
+		//#pragma endregion
 
-		// Right vector
-		glm::vec3 right = glm::vec3(
-			sin(horizontalAngle - 3.14f / 2.0f),
-			0,
-			cos(horizontalAngle - 3.14f / 2.0f)
-		);
+		//#pragma region Keyboard - Position
+		//const glm::mat4& getViewMatrix() const
+		//{
+		//	return ViewMatrix;
+		//}
+		//const glm::mat4& getProjectionMatrix() const
+		//{
+		//	return ProjectionMatrix;
+		//}
 
-		// Up vector
-		glm::vec3 up = glm::cross(right, direction);
+		//static bool is_call_back_method_set_up_for_keyboard_events = false;
+		//if (!is_call_back_method_set_up_for_keyboard_events)
+		//{
+		//	glfwSetKeyCallback(window, InPut::KeyBoard::press_or_release_callback);
+		//	InPut::KeyBoard::getKey(GLFW_KEY_UP).regist(m_observer_up);
+		//	InPut::KeyBoard::getKey(GLFW_KEY_DOWN).regist(m_observer_down);
+		//	InPut::KeyBoard::getKey(GLFW_KEY_RIGHT).regist(m_observer_right);
+		//	InPut::KeyBoard::getKey(GLFW_KEY_LEFT).regist(m_observer_left);
+		//	is_call_back_method_set_up_for_keyboard_events = true;
+		//}
 
-		#pragma endregion
+		//// Move forward
+		//position += direction * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_UP).getTimePressed()) * speed;
+		//// Move backward
+		//position -= direction * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_DOWN).getTimePressed()) * speed;
+		//// Strafe right
+		//position += right * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_RIGHT).getTimePressed()) * speed;
+		//// Strafe left
+		//position -= right * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_LEFT).getTimePressed()) * speed;
 
-		#pragma region Keyboard - Position
-		const glm::mat4& getViewMatrix() const
-		{
-			return ViewMatrix;
-		}
-		const glm::mat4& getProjectionMatrix() const
-		{
-			return ProjectionMatrix;
-		}
+		//#pragma endregion
 
-		static bool is_call_back_method_set_up_for_keyboard_events = false;
-		if (!is_call_back_method_set_up_for_keyboard_events)
-		{
-			glfwSetKeyCallback(window, InPut::KeyBoard::press_or_release_callback);
-			InPut::KeyBoard::getKey(GLFW_KEY_UP).regist(m_observer_up);
-			InPut::KeyBoard::getKey(GLFW_KEY_DOWN).regist(m_observer_down);
-			InPut::KeyBoard::getKey(GLFW_KEY_RIGHT).regist(m_observer_right);
-			InPut::KeyBoard::getKey(GLFW_KEY_LEFT).regist(m_observer_left);
-			is_call_back_method_set_up_for_keyboard_events = true;
-		}
+		//#pragma region ScrollBar - Angle
 
-		// Move forward
-		position += direction * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_UP).getTimePressed()) * speed;
-		// Move backward
-		position -= direction * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_DOWN).getTimePressed()) * speed;
-		// Strafe right
-		position += right * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_RIGHT).getTimePressed()) * speed;
-		// Strafe left
-		position -= right * static_cast<float>(InPut::KeyBoard::getKey(GLFW_KEY_LEFT).getTimePressed()) * speed;
+		//static bool is_call_back_method_set_up_for_scrolling = false;
+		//if (!is_call_back_method_set_up_for_scrolling)
+		//{
+		//	glfwSetScrollCallback(window, InPut::ScrollBar::scrollCallBack);
+		//	InPut::ScrollBar::regist(*this);
+		//	is_call_back_method_set_up_for_scrolling = true;
+		//}
 
-		#pragma endregion
+		//#pragma endregion
 
-		#pragma region ScrollBar - Angle
-
-		static bool is_call_back_method_set_up_for_scrolling = false;
-		if (!is_call_back_method_set_up_for_scrolling)
-		{
-			glfwSetScrollCallback(window, InPut::ScrollBar::scrollCallBack);
-			InPut::ScrollBar::regist(*this);
-			is_call_back_method_set_up_for_scrolling = true;
-		}
-
-		#pragma endregion
-
-		// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-		ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
-		// Camera matrix
-		ViewMatrix = glm::lookAt(
-			position,           // Camera is here
-			position + direction, // and looks here : at the same position, plus "direction"
-			up                  // Head is up (set to 0,-1,0 to look upside-down)
-		);
+		//// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+		//ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+		//// Camera matrix
+		//ViewMatrix = glm::lookAt(
+		//	position,           // Camera is here
+		//	position + direction, // and looks here : at the same position, plus "direction"
+		//	up                  // Head is up (set to 0,-1,0 to look upside-down)
+		//);
 
 	}
 
