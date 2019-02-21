e8505575bd336cbab3b82124c3d2472329dabb68
DanielSandor84 <sandordaniel19840727@gmail.com>
2019-02-21 23:48:09 +0100

    Eliminating the need for ambient light coefficient

--- Light.cpp
+++ Light.cpp
@@ -4,7 +4,6 @@
 std::vector < Light* >    Light::lights{};
 
 std::vector < GLfloat >   Light::m_source_light_power{};
-std::vector < GLfloat >   Light::m_source_light_ambient_power_coefficient{};
 std::vector < glm::vec4 > Light::m_source_light_posdir{};
 
 std::vector < GLfloat >   Light::m_positional_light_angle_in_radians{};
@@ -15,7 +14,6 @@ std::vector < glm::vec3 > Light::m_source_light_specular_col{};
 std::vector < glm::vec3 > Light::m_source_light_ambient_col{};
 
 GLint Light::m_source_light_powerID;
-GLint Light::m_source_light_ambient_power_coefficientID;
 GLint Light::m_source_light_posdirID;
 
 GLint Light::m_positional_light_angle_in_radiansID;
--- Light.h
+++ Light.h
@@ -20,7 +20,6 @@ public:
 	static std::vector < Light* >    lights;
 
 	static std::vector < GLfloat >   m_source_light_power;
-	static std::vector < GLfloat >   m_source_light_ambient_power_coefficient;
 	static std::vector < glm::vec4 > m_source_light_posdir;
 
 	static std::vector < GLfloat >   m_positional_light_angle_in_radians;
@@ -31,7 +30,6 @@ public:
 	static std::vector < glm::vec3 > m_source_light_ambient_col;
 
 	static GLint m_source_light_powerID;
-	static GLint m_source_light_ambient_power_coefficientID;
 	static GLint m_source_light_posdirID;
 
 	static GLint m_positional_light_angle_in_radiansID;
@@ -44,7 +42,6 @@ public:
 	static void getUniformLocationsForAll(GLint m_programID)
 	{
 		m_source_light_powerID = glGetUniformLocation(m_programID, "source_light_power");
-		m_source_light_ambient_power_coefficientID = glGetUniformLocation(m_programID, "source_light_ambient_power_coefficient");
 		m_source_light_posdirID = glGetUniformLocation(m_programID, "source_light_posdir");
 		m_positional_light_angle_in_radiansID = glGetUniformLocation(m_programID, "positional_light_angle_in_radians");
 		m_positional_source_light_dirID = glGetUniformLocation(m_programID, "positional_source_light_dir");
@@ -56,7 +53,6 @@ public:
 	static void assignUniformsForAll()
 	{
 		glUniform1fv(m_source_light_powerID, m_source_light_power.size(), &m_source_light_power[0]);
-		glUniform1fv(m_source_light_ambient_power_coefficientID, m_source_light_power.size(), &m_source_light_ambient_power_coefficient[0]);
 		glUniform4fv(m_source_light_posdirID, m_source_light_power.size(), &m_source_light_posdir[0][0]);
 		glUniform1fv(m_positional_light_angle_in_radiansID, m_source_light_power.size(), &m_positional_light_angle_in_radians[0]);
 		glUniform3fv(m_positional_source_light_dirID, m_source_light_power.size(), &m_positional_source_light_dir[0][0]);
@@ -78,7 +74,6 @@ public:
 		lights.push_back(this);
 
 		m_source_light_power.push_back(0);
-		m_source_light_ambient_power_coefficient.push_back(0);
 		m_source_light_posdir.push_back(glm::vec4(0.0f, 10.0f, 0.0f, 1.0f));
 
 		m_positional_light_angle_in_radians.push_back(2 * glm::pi<float>());
@@ -96,7 +91,6 @@ public:
 		}
 
 		m_source_light_power.erase(m_source_light_power.begin() + id);
-		m_source_light_ambient_power_coefficient.erase(m_source_light_ambient_power_coefficient.begin() + id);
 		m_source_light_posdir.erase(m_source_light_posdir.begin() + id);
 
 		m_positional_light_angle_in_radians.erase(m_positional_light_angle_in_radians.begin() + id);
@@ -110,10 +104,6 @@ public:
 	void setPower(const GLfloat a)
 	{
 		m_source_light_power[id] = a;
-	}
-	void setAmbientPowerCoefficient(const GLfloat a)
-	{
-		m_source_light_ambient_power_coefficient[id] = a;
 	}
 	void setPosDir(const glm::vec4& a)
 	{
