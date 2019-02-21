080a542a016a779bd9240c34f989a8141c0211b1
DanielSandor84 <sandordaniel19840727@gmail.com>
2019-02-21 23:55:20 +0100

    Correcting TBN

--- TangentSpace.cpp
+++ TangentSpace.cpp
@@ -1,5 +1,4 @@
 #include <glm/glm.hpp>
-#include <glm/gtx/transform.hpp>
 
 #include "TangentSpace.hpp"
 
@@ -34,10 +33,10 @@ void computeTangentBasis(
 
 		// Set the same tangent for all three vertices of the triangle.
 		compressed_tangents[inds[i + 0]] = compressed_tangents[inds[i + 1]] = compressed_tangents[inds[i + 2]] = 
-			-glm::vec3(glm::rotate(glm::mat4(), glm::radians<float>(90), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(glm::normalize(tangent), 1.0f)); // TODO: Find out why do we have to rotate tangent and bitangent by 90 degrees around y axe and after that multiply tangent by -1.
+			glm::normalize(tangent);
 
 		// Same thing for binormals
 		compressed_bitangents[inds[i + 0]] = compressed_bitangents[inds[i + 1]] = compressed_bitangents[inds[i + 2]] = 
-			glm::vec3(glm::rotate(glm::mat4(), glm::radians<float>(90), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(glm::normalize(bitangent), 1.0f));
+			glm::normalize(bitangent);
 	}
 }
