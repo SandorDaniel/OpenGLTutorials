827147de678b6674685a53659d561c41d1bd9306
DanielSandor84 <sandordaniel19840727@gmail.com>
2019-03-04 15:47:23 +0100

    Correction of glm::vec3 getDir(const float m_vertical_angle, const float m_horizontal_angle);

--- Math.cpp
+++ Math.cpp
@@ -14,6 +14,6 @@ glm::vec3 getDir(const float m_vertical_angle, const float m_horizontal_angle)
 glm::vec2 getVerticalAndHorizontalAngles(const glm::vec3& v)
 {
 	return glm::vec2(
-		glm::atan(v.x / v.z),
+		glm::atan(v.x, v.z),
 		glm::asin(v.y / glm::length(v)));
 }
